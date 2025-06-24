935afc97d66636c3e54abd8cc59c044fb21546f6
gkalinnikov <gkalinnikov@gmail.com>
2025-06-24 19:00:02 +0300

    Калькулятор

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длинна текста: " + text.length());
+        System.out.println("Введите число:");
+        int firstNumber = new Scanner(System.in). nextInt();
+        int secondNumber = new Scanner(System.in). nextInt();
+
+        double quotient = (double) firstNumber / secondNumber;
+        double sum = (double) firstNumber + secondNumber;
+        double difference = (double) firstNumber - secondNumber;
+        double multiplication = (double) firstNumber * secondNumber;
+
+        System.out.println("Частное: " + quotient);
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + multiplication);
+
 
     }
 }
